153dbeea7653261033127c76b9dd0438d649b6d5
vasya <vasya@vasya.com>
2016-08-08 14:17:22 +0400

    final

--- week5-dijkstra-shortest-path/main.c
+++ week5-dijkstra-shortest-path/main.c
@@ -8,6 +8,7 @@
 #define TEST 0
 #define TEST_INPUT_FILE "test.txt"
 #define INPUT_FILE "dijkstraData.txt"
+#define STARTING_NODE 1
 
 typedef struct
 {
@@ -39,7 +40,7 @@ void readInput(Graph *g);
 void dijkstraSP(Graph *g);
 void addNode();
 Edge* pickMinEdge(int *A, CrosEdges *ce);
-void fixCrossEdges(CrosEdges *ce, char *visited);
+void fixCrossEdges(CrosEdges *ce, char *visited);   //delete edges that became internal for visited nodes
 void printEdge(Edge *edge);
 
 int main()
@@ -49,7 +50,9 @@ int main()
     Graph g;
     g.n = 0;
     g.m = 0;
+
     readInput(&g);
+
     printf("[!] Number of vertices - %d\n", g.n);
     printf("[!] Number of edges    - %d\n", g.m);
 
@@ -61,7 +64,6 @@ int main()
 void readInput(Graph* g)
 {
     FILE *f;
-    int i, j;
 #if TEST
     f = fopen(TEST_INPUT_FILE, "r");
 #else
@@ -70,10 +72,7 @@ void readInput(Graph* g)
 
     char buffer[200000];
     while (fgets(buffer, sizeof(buffer), f))
-    {
-        //printf("%s\n", buffer);
         g->n++;
-    }
     rewind(f);
 
     g->nodes = malloc(sizeof(Edges*)*g->n);
@@ -107,24 +106,12 @@ void readInput(Graph* g)
             g->nodes[node]->sz++;
         }
     }
-
-/*
-    for (i = 1; i <= g->n; i++)
-    {
-        printf("#%d %d:\n", i, g->nodes[i].edges[0].s);
-        for (j = 0; j < g->nodes[i].sz; j++)
-        {
-            Edge* tempE;
-            tempE = g->nodes[i].edges + j;
-            printf("\t %d %d\n", tempE->e, tempE->ln);
-        }
-    }
-*/
 }
 
 void dijkstraSP(Graph *g)
 {
     int i;
+
     int A[g->n + 1];
     char visited[g->n+ 1];
     for (i = 0; i <= g->n; i++)
@@ -137,30 +124,21 @@ void dijkstraSP(Graph *g)
     ce.edges = malloc(sizeof(Edge)*g->m);
     ce.sz = 0;
 
+    /* Init with starting node */
     Edge *tempEdge;
     tempEdge = malloc(sizeof(Edge));
     tempEdge->s = 1;
     tempEdge->e = 1;
     tempEdge->ln = 0;
 
-    addNode(g, visited, 1, A, tempEdge, &ce);
+    addNode(g, visited, STARTING_NODE, A, tempEdge, &ce);
 
+    /* Main loop*/
     for (i = 0; i < g->n - 1; i++)
     {
         Edge *minEdge;
         minEdge = pickMinEdge(A, &ce);
-        printEdge(minEdge);
         addNode(g, visited, minEdge->e, A, minEdge, &ce);
-    }
-
-    for (i = 0; i < ce.sz; i++)
-    {
-        printf("#%d %d %d %d \n", i, ce.edges[i].s, ce.edges[i].e, ce.edges[i].ln);
-    }
-
-    for (i = 0; i <= g->n; i++)
-    {
-        printf("#%d %d %d %d \n", i, A[i], visited[i]);
     }
 
     printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", A[7],A[37],A[59],A[82],A[99],A[115],A[133],A[165],A[188],A[197]);
@@ -177,8 +155,6 @@ void addNode(Graph *g, char *visited, int index, int *A, Edge* edge, CrosEdges *
     Edges *edges;
     edges = g->nodes[index];
 
-    printf("%d %d \n", index, edges->sz);
-
     for (i = 0; i < edges->sz; i++)
     {
         if (visited[edges->edges[i].e] == 0)
@@ -188,20 +164,21 @@ void addNode(Graph *g, char *visited, int index, int *A, Edge* edge, CrosEdges *
             //printf("s %d %d \n", edge->s, ce->sz);
         }
     }
+
     fixCrossEdges(ce, visited);
 };
 
 Edge* pickMinEdge(int *A, CrosEdges *ce)
 {
     int i;
+
     Edge *minEdge;
     minEdge = ce->edges;
+
     for (i = 0; i < ce->sz; i++)
     {
         if (minEdge->ln + A[minEdge->s]> ce->edges[i].ln + A[ce->edges[i].s])
-        {
             minEdge = ce->edges + i;
-        }
     }
 
     return minEdge;
